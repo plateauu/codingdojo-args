26c450a0b5ff90df9185b6c9a2d1183e4002758a
plateauu <plateauu@o2.pl>
2020-03-27 20:55:26 +0100

    dziz..

---
+++ src/test/groovy/tech.plateauu.validator/ArgumentSchemaValidatorGroovyTest.groovy
@@ -0,0 +1,12 @@
+package tech.plateauu.validator
+
+import spock.lang.Specification
+
+class ArgumentSchemaValidatorGroovyTest extends Specification {
+
+    def "example test"(){
+        expect:
+        throw new RuntimeException()
+    }
+
+}
