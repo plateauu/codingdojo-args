f7ea15e48d8d97444089f9eaf74bf6dc04031fe2
plateauu <plateauu@o2.pl>
2020-05-06 21:45:15 +0200

    Polishing

--- src/main/java/tech/plateauu/validator/core/definition/JsonDefinitionParserUtil.java
+++ src/main/java/tech/plateauu/validator/core/definition/JsonDefinitionParserUtil.java
@@ -1,10 +1,6 @@
 package tech.plateauu.validator.core.definition;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.type.CollectionType;
-import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import tech.plateauu.validator.core.flag.FlagDefinition;
 import tech.plateauu.validator.core.flag.MappedDefinition;
 
@@ -14,24 +10,19 @@ import java.util.List;
 
 class JsonDefinitionParserUtil {
 
-	private static final Logger LOG = LoggerFactory.getLogger(JsonDefinitionParserUtil.class);
-
-	public static Definition parse(File file) {
-		Definition definition = null;
+	static Definition parse(File file) {
 		try {
 			var mappedDefinition = parseMappedDefinition(file);
-			definition = new Definition(mappedDefinition);
+			return new Definition(mappedDefinition);
 		} catch (IOException e) {
 			throw new RuntimeException("Exception during mapping", e);
 		}
-		return definition;
 	}
 
-	@Nullable
 	private static List<FlagDefinition> parseMappedDefinition(File file) throws IOException {
 		var mapper = new ObjectMapper();
 		var typeFactory = mapper.getTypeFactory();
-		CollectionType collectionType = typeFactory.constructCollectionType(List.class, MappedDefinition.class);
+		var collectionType = typeFactory.constructCollectionType(List.class, MappedDefinition.class);
 		return mapper.readValue(file, collectionType);
 	}
 }
